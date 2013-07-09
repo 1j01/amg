697503045340f718ef6ade891acc2fbf01743c19
Isaiah Odhner <isaiahodhner@gmail.com>
2013-07-09 09:00:06 +0000

    Art Assets; Art Manager; improved Pixel Editor

---
+++ listcontent.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+import os
+print "Content-Type: text/plain\n\n"
+for root, dirs, files in os.walk("content"):
+    for file in files:
+		if not file.endswith(('.md')):
+			print os.path.join(root, file)
