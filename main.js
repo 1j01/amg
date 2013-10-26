4420450ba8c5a47664e83815889a95192672b6cf
Isaiah Odhner <isaiahodhner@gmail.com>
2013-10-26 16:20:43 +0000

    synk

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
 
-const TS = 32;
+TS = 32;
 
 //(function(_main_){
 	var canvas = document.querySelector("canvas");
 	var mainctx = canvas.getContext("2d");
 	addEventListener('contextmenu',function(e){return!!e.preventDefault()});
 	//addEventListener('mousedown',function(e){return!!e.preventDefault()});
-	
 	var editing = true;
+	
 	var gui = new GUI();
+	
+	var u, le, game;
 	
 	var aa = new ArtAssets(mainctx, whenceBeAssetsLoaded, whenceAnUpdateDothForsooth);
 	var am = new ArtManager(aa, gui);
 	
-	var u, le, game;
-	
+	function whenceAnUpdateDothForsooth(){
+		am.update();
+	}
 	function whenceBeAssetsLoaded(){
 		console.log("Assets loaded.");
 		am.update();
-		try {
+		try{
 			u = new Universe(mainctx);
 			var room = new Room("starting area",2,2,38/2,24/2,aa);
 			room.rows[6][5]={
@@ -30,20 +33,19 @@ const TS = 32;
 			u.addRoom(room2);
 			le = new LevelEditor(u, mainctx);
 			game = new Game(u, mainctx);
-		} catch(e){
+		}catch(e){
 			gui.M().title("Something didn't work!").content(e.message).position("bottom");
 		}
 	}
-	function whenceAnUpdateDothForsooth(){
-		am.update();
-	}
-	
-	if(window.requestAnimationFrame){
-		requestAnimationFrame(function animate(){
-			requestAnimationFrame(animate);
+	var req = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame;
+	if(req){
+		var animate = function(){
+			req(animate);
 			step();
-		});
+		};
+		req(animate);
 	}else{
+		console.log("Wow, no requestAnimationFrame? Lamesauce.");
 		setTimeout(step,10);
 	}
 	
@@ -64,38 +66,40 @@ const TS = 32;
 	var mainScreen = null;
 	var mainScreenTurnOn = function(){
 		mainScreen = gui.M()
-			.title("Controls")
-			.position("center")
-			.content(
-				"<button id='show-art-manager'>Show Art Asset Manager</button>"+
-			"<br><button id='open-pixel-editor'>Open Pixel Editor</button>"+
-			"<br><button id='about'>About</button>"
-			).$("#show-art-manager",function($e){
-				$e.onclick = function(e){
-					am.show();
-				};
-			}).$("#open-pixel-editor",function($e){
-				$e.onclick = function(e){
-					new PixelEditor(gui,null,function(img){
-						console.log("~update(img){ window.unsaved = img.src; }()");
-						window.unsaved = img.src;
-					});
-				};
-			}).$("#about",function($e){
-				$e.onclick = function(e){
-					var m=gui.M().title("About").position("center").content(
-						"<p>This is an abstracted modular game framework with integrated pixel editor and realtime asset manager and a level editor with dynamic block type definitions and a skeletal character animator and inverse kinematics and a game.</p>"
-						+"<p>Well, so far just a pixel editor and realtime asset manager...</p>"
-						+"<p>...</p>"
-						+"<p><a href='https://github.com/1j01/amg'>It's on github.</a></p>"
-					);
-					m.$m.style.width="500px";
-					m.$m.style.maxWidth="80vw";
-					m.$c.style.maxHeight="80vh";
-					m.$c.style.overflowY="auto";
-					m.$c.style.overflowX="auto";
-				};
-			});
+		.title("Controls")
+		.position("center")
+		.content(
+			"<button id='show-art-manager'>Show Art Asset Manager</button>"+
+		"<br><button id='open-pixel-editor'>Open Pixel Editor</button>"+
+		"<br><button id='about'>About</button>"
+		).$("#show-art-manager",function($e){
+			$e.onclick = function(e){
+				am.show();
+			};
+		}).$("#open-pixel-editor",function($e){
+			$e.onclick = function(e){
+				new PixelEditor(gui,null,function(img){
+					console.log("window.unsaved = img.src;");
+					window.unsaved = img.src;
+				},function(img, shiftKey){
+					window.open(img.src);
+				});
+			};
+		}).$("#about",function($e){
+			$e.onclick = function(e){
+				var m=gui.M().title("About").position("center").content(
+					"<p>This is an abstracted modular game framework with integrated pixel editor and realtime asset manager and a level editor with dynamic block type definitions and a skeletal character animator and inverse kinematics and a game.</p>"
+					+"<p>Well, so far just a pixel editor and realtime asset manager...</p>"
+					+"<p>...</p>"
+					+"<p><a href='https://github.com/1j01/amg'>It's on github.</a></p>"
+				);
+				m.$m.style.width="500px";
+				m.$m.style.maxWidth="80vw";
+				m.$c.style.maxHeight="80vh";
+				m.$c.style.overflowY="auto";
+				m.$c.style.overflowX="auto";
+			};
+		});
 		mainScreen.onclose = function(){
 			mainScreen=false;
 			return true;
@@ -114,7 +118,7 @@ const TS = 32;
 			}
 		}
 	});
-	//var pe = new PixelEditor(gui);
+	//var pe = new PixelEditor(gui,..?);
 	mainScreenTurnOn();
 //})();
 
